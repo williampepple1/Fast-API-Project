b60937fbbe0fa5640c5fa993612ddecf1fcce8b9
William Pepple <78800029+williampeppleBG@users.noreply.github.com>
2022-06-24 16:10:57 +0100

    Oauth Implemented

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
@@ -35,3 +36,11 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
