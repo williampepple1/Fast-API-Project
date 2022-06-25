ca9c22b5fb813f37d569824b4442480b8c1ac82c
William Pepple <78800029+williampeppleBG@users.noreply.github.com>
2022-06-25 10:13:08 +0100

    Basic logic completed

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from fastapi import Body, Depends, FastAPI, Response, status, HTTPException, APIRouter
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from .. import models, schemas, oauth2
 from ..database import get_db
@@ -9,12 +10,18 @@ router = APIRouter(
     tags=['Posts']
 )
 
-@router.get('/', response_model=List[schemas.Post])
+@router.get('/', response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db),  current_user: int = Depends(oauth2.get_current_user), limit: int=10 , skip: int = 0, search: Optional[str] =""):
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
 # for only the users post ||  posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
-    return  posts
+
+    # return  posts
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    print (results)
+    return results
+
+
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
@@ -27,9 +34,12 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current
     return new_post
 
 
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Post)
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.PostOut)
 def get_post(id: int,  db: Session = Depends(get_db),  current_user: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    # post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id{id} was not found")
     return  post
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
 
 
@@ -36,7 +36,7 @@ class PostCreate(PostBase):
 
 
 class Post(PostBase):
-    id: str
+    id: int
     created_at: datetime
     owner_id: int
     owner: UserOut
@@ -45,6 +45,12 @@ class Post(PostBase):
         orm_mode = True
 
 
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        orm_mode = True
 
 
 
@@ -54,3 +60,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
